9726684fc7e5855fa5d2a79c6e5f07fb126aeffb
robscott3 <robertsc@spc.int>
2015-07-20 14:09:16 +1100

    new bio_len plots
    still an issue with Porbeagle yat to sort out

--- CODE/LL Median Length and Maturity_RDS_separate because Joel checking data and may need to revert to original.r
+++ CODE/LL Median Length and Maturity_RDS_separate because Joel checking data and may need to revert to original.r
@@ -42,13 +42,20 @@ load(file="C:/wcpfc/shark indicators/shk-indicators-2015/DATA/reconciled_catch.r
  sizeMatFS=c(168,275,144,173,203)                      #females in the South Pacific                         
  sizeMatMN=c(168,180,138,175,168)                      #males in the North Pacific
  sizeMatMS=c(168,180,138,175,168)                      #males in the South Pacific
-    
+ 
+
+
+
 #  
 sexid<- c("Male", "Female")
 group_names <-c("Blue", "Mako", "Oceanic Whitetip", "Silky", "Thresher", "Hammerhead", "Porbeagle") 
 gender <- c("M", "F")
 #sppNames <- c("OWT","Blue","Thresher","Mako","Silky")
 sppNames <- c('BSH','OCS','THR','MAK','FAL','HHD','POR')
+
+sizeMat <- array(c(c(168,144,203,275,173,210,175),
+                   c(168,138,168,180,175,198,145)), dim=c(7,2), dimnames=list(sppNames,c("F","M")))
+
 #---------------------------------------------------------------------------------------------------
 #---------------------------------------------------------------------------------------------------
 #  Subset data and plot
@@ -81,7 +88,7 @@ res.df[res.df$sex=='M','Sex']<- 'Males'
 res.df[res.df$sex=='F','Sex']<- 'Females'
 
 
-sp <- sppNames[5]
+sp <- sppNames[7]
 nyrs <- length(with(res.df, subset(dat,  spp==sp & sex=='F' & type=='lower')))/5
 
 poly.df <- data.frame(Region=rep(c(2:6,6:2), each=nyrs),sex=rep(c('F','M'),each=nyrs*10), 
@@ -99,23 +106,6 @@ poly.df <- data.frame(Region=rep(c(2:6,6:2), each=nyrs),sex=rep(c('F','M'),each=
 poly.df$poly.count <- 1
 poly.df$poly.count[is.na(poly.df$yvals)] <- NA
 
- #   shh <- subset(poly.df, Region==rgg & sex==sxx)
-#    orig.poly.count <- shh$poly.count
-#    orig.first <- orig.poly.count[1]
-#    shh$poly.count[1] <- 1
-#    for(ii in 1:nyrs){
-#      if(is.na(shh$poly.count[ii])){
-#        shh$poly.count[(ii+1):nyrs] <- shh$poly.count[ii-1]+1
-#        shh$poly.count[is.na(orig.poly.count)] <- NA
-#      }
-#    }
-#    shh$poly.count[1] <- orig.first
-#    shh$poly.count[(nyrs+1):(nyrs*2)] <- rev(shh$poly.count[1:nyrs])
-#    poly.df[poly.df$Region==rgg & poly.df$sex==sxx,]$poly.count <- shh$poly.count
-#  }
-#}
-
-
 for(rgg in 2:6){
   for(sxx in c('F','M')){
     kk <- subset(poly.df, Region==rgg & sex==sxx)$poly.count[1:nyrs]
@@ -131,6 +121,9 @@ for(rgg in 2:6){
   }
 }
 
+
+filename <- paste("FIG_XX_bio_len_",sp,"_RDS.png", sep="")
+png(paste("C:/wcpfc/shark indicators/shk-indicators-2015/GRAPHICS/",filename,sep=""), width=900, height=700)
 
 
 pfun <- function(x, y, Lmat, nrec, polygons, ...){
@@ -150,12 +143,99 @@ sb <- trellis.par.get("strip.background")
 sb$col[c(1,2)] <- c('ivory2','ivory3')
 trellis.par.set("strip.background", sb)
 
-
-
 xyplot(dat~year|Sex*as.character(region), groups=type, data=res.df[res.df$spp==sp,], type='l', lty=c(2,1,2),
-       layout=c(2,6),as.table=T, Lmat=rep(168,12), panel=pfun, polygons=poly.df,
+       layout=c(2,6),as.table=T, Lmat=rep(sizeMat[sp,],6), 
+       panel=pfun, polygons=poly.df,
        nrec=c(nrecords[c(paste(sp,'M',sep='_'),paste(sp,'F',sep='_')),]), 
        ylab="Median Upper Jaw-Fork Length", xlab="", scales=list(x=list(alternating=F)),ylim=c(0,350))
+
+dev.off()
+
+
+
+########################
+##
+## PORBEAGLE ONLY
+##
+
+sp <- sppNames[7]
+nyrs <- length(with(res.df, subset(dat,  spp==sp & sex=='F' & type=='lower')))/2
+
+poly.df <- data.frame(Region=rep(c(5:6,6:5), each=nyrs),sex=rep(c('F','M'),each=nyrs*2), 
+                      panelct=c(rep(c(3,5,7,9,11,11,9,7,5,3),each=nyrs),rep(c(4,6,8,10,12,12,10,8,6,4),each=nyrs)),
+                      yvals =   c(with(res.df, subset(dat,  spp==sp & sex=='F' & type=='lower')),
+                                  rev(with(res.df, subset(dat,  spp==sp & sex=='F' & type=='upper'))),
+                                  with(res.df, subset(dat,  spp==sp & sex=='M' & type=='lower')),
+                                  rev(with(res.df, subset(dat,  spp==sp & sex=='M' & type=='upper')))),
+                      xvals =   c(with(res.df, subset(year, spp==sp & sex=='F' & type=='lower')),
+                                  rev(with(res.df, subset(year, spp==sp & sex=='F' & type=='upper'))),
+                                  with(res.df, subset(year, spp==sp & sex=='M' & type=='lower')),
+                                  rev(with(res.df, subset(year, spp==sp & sex=='M' & type=='upper')))))
+
+#for regions and sexes with NAs work out how many polygons need to be drawn and number them appropriateley
+poly.df$poly.count <- 1
+poly.df$poly.count[is.na(poly.df$yvals)] <- NA
+
+for(rgg in :6){
+  for(sxx in c('F','M')){
+    kk <- subset(poly.df, Region==rgg & sex==sxx)$poly.count[1:nyrs]
+    kk.orig <- kk
+    for(ii in 1:nyrs){
+      if(is.na(kk)[ii]){
+        kk[(ii+1):nyrs] <- ii
+        kk[is.na(kk.orig)] <- NA
+      }
+    }
+    kk <- kk[1:nyrs]
+    poly.df[poly.df$Region==rgg & poly.df$sex==sxx,]$poly.count <- c(kk, rev(kk))
+  }
+}
+
+
+filename <- paste("FIG_XX_bio_len_",sp,"_RDS.png", sep="")
+png(paste("C:/wcpfc/shark indicators/shk-indicators-2015/GRAPHICS/",filename,sep=""), width=900, height=700)
+
+
+pfun <- function(x, y, Lmat, nrec, polygons, ...){
+  ppp <- polygons[polygons$panelct==panel.number(),]
+  for(pcounter in 1:20){ #max(ppp$poly.count)){
+    xxx <- ppp[ppp$poly.count==pcounter,]$xvals
+    yyy <- ppp[ppp$poly.count==pcounter,]$yvals
+    panel.polygon(xxx[!is.na(xxx)], yyy[!is.na(yyy)], col='lightgrey',lty='n',border=NA,...)
+  }
+  
+  panel.abline(h=Lmat[panel.number()], col='darkgrey', lty=2)
+  panel.xyplot(x,y,col=c('lightgrey','black','lightgrey'),...)
+  panel.text(1995,290, paste("n =",nrec[panel.number()]), col='black')
+}
+
+sb <- trellis.par.get("strip.background")
+sb$col[c(1,2)] <- c('ivory2','ivory3')
+trellis.par.set("strip.background", sb)
+
+xyplot(dat~year|Sex*as.character(region), groups=type, data=res.df[res.df$spp==sp,], type='l', lty=c(2,1,2),
+       layout=c(2,6),as.table=T, Lmat=rep(sizeMat[sp,],6), 
+       panel=pfun, polygons=poly.df,
+       nrec=c(nrecords[c(paste(sp,'M',sep='_'),paste(sp,'F',sep='_')),]), 
+       ylab="Median Upper Jaw-Fork Length", xlab="", scales=list(x=list(alternating=F)),ylim=c(0,350))
+
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
